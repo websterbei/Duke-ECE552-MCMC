302e4c82455aab19eebabee0117e9c218ca51465
Webster Bei Yijie <yijie.bei@duke.edu>
2019-11-12 11:09:24 -0500

    added commandline argument

--- MCMC_CPU/main.cpp
+++ MCMC_CPU/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "distribution_function.h"
 #include "metropolis_hastings.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
     const int dimension = 2;
-    const int num_samples = 100;
+    const int num_samples = argc>1 ? atoi(argv[1]) : 100;
     double** samples = (double**)malloc(num_samples * sizeof(double*));
     for(int i=0; i<num_samples; i++) {
         samples[i] = (double*)malloc(dimension * sizeof(double));
     }
     metropolis_hastings(distribution_function, num_samples, dimension, samples);
+
+    ofstream output_file;
+    output_file.open("samples.csv");
     for(int i=0; i<num_samples; i++) {
-        cout<<samples[i][0]<<" "<<samples[i][1]<<endl;
+        for(int j=0; j<dimension-1; j++) {
+            output_file<<samples[i][j]<<",";
+        }
+        output_file<<samples[i][dimension]<<"\n";
     }
+    output_file.close();
     return 0;
 }
